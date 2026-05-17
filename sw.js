9ff03eaf0826367f35012d423a1e5424937a3e5e
itsapieceofcake4-max <its.a.piece.of.cake4@gmail.com>
2026-05-17 21:35:39 +0900

    Add PWA support: manifest, service worker, icons

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'running-form-v1';
+const STATIC = ['./motion_capture_app.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(STATIC)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  // CDN resources: network first, fallback to cache
+  if (url.origin !== location.origin) {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+    return;
+  }
+  // Local files: cache first
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }))
+  );
+});
